d976ed252992452ddb739effd7302f43e8c2339d
Leigh <leighbryant@bellsouth.net>
2016-11-17 19:19:54 -0500

    merge conflict resolution

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,33 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  headerMessage: 'Coming Soon',
-  responseMessage: '',
-  emailAddress: '',
+ headerMessage: 'Coming Soon',
+ responseMessage: '',
+ emailAddress: '',
 
-  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid'),
+ isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+ isDisabled: Ember.computed.not('isValid'),
 
-  actions: {
+ actions: {
 
-    saveInvitation() {
+   saveInvitation() {
 
-      var self = this;
-      var email = this.get('emailAddress');
+     var self = this;
+     var email = this.get('emailAddress');
 
-      var newInvitation = this.store.createRecord('invitation', {
-                 email: email
-             });
+     var newInvitation = this.store.createRecord('invitation', {
+                email: email
+            });
 
-      newInvitation.save().then(function(response) {
-        self.set('responseMessage', "Thank you!  " + response.get('emailAddress'));
-        self.set('emailAddress', '');
-             });
-    }
-
-    getDate () {
-      
-    }
-  }
-
+     newInvitation.save().then(function(response) {
+       self.set('responseMessage', "Thank you!  " + response.get('emailAddress'));
+       self.set('emailAddress', '');
+            });
+   }
+ }
 });
