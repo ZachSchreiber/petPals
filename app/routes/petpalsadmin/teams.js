ac15715a313cb2448f28dc7a840cbe5e39e0c3e7
Zachary Schreiber <zachgives@gmail.com>
2016-11-18 16:16:45 -0500

    Nothing done

--- app/routes/petpalsadmin/teams.js
+++ app/routes/petpalsadmin/teams.js
@@ -3,17 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    return this.store.createRecord('blog-post');
-  },
-
-  model() {
     return this.store.createRecord('teams');
   },
 
-
   actions: {
 
-    savePost(newPost) {
+    saveTeam(newPost) {
       newPost.save().then(() => this.transitionTo('petpalsadmin'));
     },
 
@@ -21,11 +16,6 @@ export default Ember.Route.extend({
       // rollbackAttributes() removes the record from the store
       // if the model 'isNew'
       this.controller.get('model').rollbackAttributes();
-    },
-
-    saveTeam(newPost) {
-      newPost.save().then(() => this.transitionTo('petpalsadmin'));
     }
-
   }
 });
