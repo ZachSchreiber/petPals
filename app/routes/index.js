bd96f2df0b9f6b4c0dacd496edd052776963744a
Zachary Schreiber <zachgives@gmail.com>
2016-11-27 13:41:29 -0500

    nav improvements. Sort not working

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('blog-post', { reload: true }).
-      then(posts => posts.sortBy('date'));
+     return this.store.findAll('blog-post', { reload: true })
+      .then(post => post.sortBy('title:asc'));
   },
 
   session: Ember.inject.service('session'),
